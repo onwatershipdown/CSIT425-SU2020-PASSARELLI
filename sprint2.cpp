53fe0b908355194dad46d364911a0c105ec242f0
matt passarelli <pass3805@fredonia.edu>
2021-07-26 01:15:46 +0000

    version 1.1

--- sprint2.cpp
+++ sprint2.cpp
@@ -8,8 +8,8 @@ class CarRental
 
 
 private:
-	int bodytype; //1 for convertible, 2 for coupe, 3 for sedan, 4 for liftback, 5 for van, 6 for suv, 7 for truck
-	int doorcount; //2 for coupe, 3 for 2-door hatch, 4 for sedan, 5 for 4-door hatch
+	int bodytype=2; //1 for convertible, 2 for coupe, 3 for sedan, 4 for liftback, 5 for van, 6 for suv, 7 for truck
+	int doorcount=2; //2 for coupe, 3 for 2-door hatch, 4 for sedan, 5 for 4-door hatch
 	bool electric=1;
 	bool manualtrans=1;
 	int drivetype=1; //1 for front, 2 for rear, 3 for awd, 4 for 4wd
@@ -22,27 +22,33 @@ public:
 
 	void getbodytype()
 	{
-		cout<< bodytype;
+		cout<< bodytype<< endl;
 	}
-	void getdoorcount();
+	void getdoorcount()
 	{
-			
+		cout<<doorcount<< endl;	
 	}	
-	void getelectric();
+	void getelectric()
+	{
+		cout << electric << endl;
+	}
 	void gettransmission();
+	{
+		cout << manualtrans << endl;
+	}	
 	void getdrivetype();
+	{
+		cout << drivetype << endl;
+	}
 	void getcylinders();
+	{
+		cout << cylinders << endl;
+	}
 	void getsunroof();
+	{
+		cout << sunroof <<endl;
+	}
 };
-
-void newentry()
-{
-	cout << "enter the number of doors: \n";
-	
-
-
-
-}
 
 
 int main()
